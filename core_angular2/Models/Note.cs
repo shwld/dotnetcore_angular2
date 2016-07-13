b120310a6315fc09dfc846f752e031aada83b0cd
shwld <shu.account@outlook.com>
2016-07-14 00:57:37 +0900

    確認用のモデルを作成

---
+++ core_angular2/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace core_angular2.Models
+{
+    public class Note
+    {
+        [Required, Key]
+        public float Id { get; set; }
+
+        [Display(Name = "ノート名"), MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
