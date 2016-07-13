b62fced405c24d60d423c12144ed33b258957227
shwld <shu.account@outlook.com>
2016-07-14 01:25:01 +0900

    Colletionをセットしてみる

---
+++ core_angular2/Migrations/20160713160140_SetDefaultCollation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace core_angular2.Migrations
+{
+    public partial class SetDefaultCollation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER DATABASE dotnetcore_angular2 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;", suppressTransaction: true);
+            migrationBuilder.Sql("ALTER DATABASE dotnetcore_angular2 COLLATE Japanese_BIN2;", suppressTransaction: true);
+            migrationBuilder.Sql("ALTER DATABASE dotnetcore_angular2 SET MULTI_USER;", suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
